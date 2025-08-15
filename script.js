05badab172431c441a825d5df7b0669f69c807f8
Prem-123Prakash <premprakash97237@gmail.com>
2025-08-15 17:26:28 +0530

    Updated About Me section, styling, and added new images

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-// Smooth scroll for nav links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+// // Smooth scroll for nav links
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+
+// Typing effect
+// var typed = new Typed('#typed', {
+//     strings: [
+//         "🚀 Software Engineer",
+//         "Data Science & AI Enthusiast",
+//         "Machine Learning",
+//         "Data Analytics",
+//         "Quality Assurance",
+//         "Research & Innovation"
+//     ],
+//     typeSpeed: 60,
+//     backSpeed: 40,
+//     loop: true
+// });
+
+// Typing effect
+var typed = new Typed('#typed', {
+    strings: [
+        "🚀 Software Engineer",
+        "Data Science & AI Enthusiast",
+        "Machine Learning",
+        "Data Analytics",
+        "Quality Assurance",
+        "Research & Innovation"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+});
+
+// Scroll change header background
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('main-header');
+    header.classList.toggle('scrolled', window.scrollY > 50);
 });
